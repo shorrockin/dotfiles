353c4ae06f3fb2d45b306de16e21aba5cbc70674
Chris Shorrock <cs@stripe.com>
2024-05-03 07:06:12 -0700

    modified fzf settings to only fire when installed via homebrew

--- .fzf.zsh
+++ .fzf.zsh
@@ -2,12 +2,12 @@
 # ---------
 if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+
+  # Auto-completion
+  # ---------------
+  [[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+  # Key bindings
+  # ------------
+  source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
