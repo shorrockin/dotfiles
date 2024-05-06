9a2fe2403c88f00767bae9aa22a01c24bfa63e84
Chris Shorrock <cs@stripe.com>
2024-05-06 08:12:30 -0700

    better harpoon bindings

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -6,5 +6,13 @@ return {
         vim.keymap.set("n", "<leader>gd", ':Git diff<CR>', { desc = 'Fugitive: [G]it [d]iff' })
         vim.keymap.set("n", "<leader>gc", ':Git commit<CR>', { desc = 'Fugitive: [G]it [c]ommit' })
         vim.keymap.set("n", "<leader>gp", ':Git push<CR>', { desc = 'Fugitive: [G]it [p]ush' })
+
+        function ConfirmRestore()
+            local choice = vim.fn.confirm("Restore Git File?", "&Yes\n&No", 2)
+            if choice == 1 then
+                vim.cmd("Gread")
+            end
+        end
+        vim.keymap.set("n", "<leader>gr", ':lua ConfirmRestore()<CR>', { desc = 'Fugitive: [G]it [R]estore File' })
     end
 }
--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -9,13 +9,13 @@ return {
             vim.keymap.set(mode, keys, func, { desc = 'Harpoon: ' .. desc })
         end
 
-        map('<leader>a', mark.add_file, '[A]dd file to Harpoon')
-        map('<C-e>', ui.toggle_quick_menu, '[E]dit Harpoon files')
+        map('<leader>ha', mark.add_file, '[A]dd file')
+        map('<leader>he', ui.toggle_quick_menu, '[E]dit file(s)')
 
-        map('<C-H>', function() ui.nav_file(1) end, 'Navigate to first file')
-        map('<C-T>', function() ui.nav_file(2) end, 'Navigate to second file')
-        map('<C-N>', function() ui.nav_file(3) end, 'Navigate to third file')
-        map('<C-S>', function() ui.nav_file(4) end, 'Navigate to fourth file')
-        map('<C-|>', function() ui.nav_file(5) end, 'Navigate to firfth file')
+        map('<C-a>', function() ui.nav_file(1) end, 'Navigate to first file')
+        map('<C-o>', function() ui.nav_file(2) end, 'Navigate to second file')
+        map('<C-e>', function() ui.nav_file(3) end, 'Navigate to third file')
+        map('<C-u>', function() ui.nav_file(4) end, 'Navigate to fourth file')
+        map('<C-i>', function() ui.nav_file(5) end, 'Navigate to firfth file')
     end
 }
