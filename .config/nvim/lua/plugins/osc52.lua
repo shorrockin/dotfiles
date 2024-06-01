4763127db520061c1a82115e62daf029e473c80e
Chris Shorrock <cs@stripe.com>
2024-06-01 10:19:07 -0700

    more osc tuning for tmux

--- .config/nvim/lua/plugins/osc52.lua
+++ .config/nvim/lua/plugins/osc52.lua
@@ -4,6 +4,12 @@ return {
 	-- be needed in nvim 10
 	"ojroques/nvim-osc52",
 	config = function()
+		require("osc52").setup({
+			max_length = 0, -- Maximum length of selection (0 for no limit)
+			silent = true, -- Disable message on successful copy
+			tmux_passthrough = true, -- Use tmux passthrough (requires tmux: set -g allow-passthrough on)
+		})
+
 		vim.api.nvim_create_autocmd("TextYankPost", {
 			callback = function()
 				if vim.v.event.operator == "y" and vim.v.event.regname == "+" then
