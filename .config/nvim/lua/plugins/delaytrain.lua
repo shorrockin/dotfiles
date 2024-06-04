3be146b277846ce7e678ba66cec03b0bb2f551af
Chris Shorrock <cs@stripe.com>
2024-06-03 22:35:53 -0700

    disabled unused plugins

--- .config/nvim/lua/plugins/delaytrain.lua
+++ .config/nvim/lua/plugins/delaytrain.lua
@@ -3,6 +3,7 @@ return {
 	-- Delaytrain: imposes key pauses on bad motion usages to train you to
 	-- develop better vim habbits.
 	"ja-ford/delaytrain.nvim",
+	enabled = false,
 	config = function()
 		require("delaytrain").setup({
 			grace_period = 3,
--- .config/nvim/lua/plugins/pretty-fold.lua
+++ .config/nvim/lua/plugins/pretty-fold.lua
@@ -2,6 +2,7 @@ return {
 	-- https://github.com/anuvyklack/pretty-fold.nvim
 	-- makes folds less ugly, maybe we don't need this, do we use folds that much?
 	"anuvyklack/pretty-fold.nvim",
+	enabled = false,
 	config = function()
 		require("pretty-fold").setup()
 	end,
