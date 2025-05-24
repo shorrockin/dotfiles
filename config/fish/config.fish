8ebe3aadf04690288c00cf5da3d687406363fdfc
Chris Shorrock <chris.shorrock@gmail.com>
2025-05-24 12:15:21 -0700

    cleanup up git abbrevs

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -24,8 +24,8 @@ abbr --add gc "git add --all . && git add -u . && git commit -am"
 abbr --add gcnv "git add --all . && git add -u . && git commit --no-verify -am"
 abbr --add gp git pull
 abbr --add gb "git reflog show --pretty=format:'%gs ~ %gd' --date=relative | grep 'checkout:' | grep -oE '[^ ]+ ~ .*' | awk -F~ '!seen[\$1]++' | head -n 10 | awk -F' ~ HEAD@{' '{printf(\"  \\033[33m%s: \\033[37m %s\\033[0m\\n\", substr(\$2, 1, length(\$2)-1), \$1)}'"
-abbr --add gundo git reset --soft HEAD && git restore --staged .
-abbr --add gamend git commit --amend
+abbr --add gundo "git reset --soft HEAD && git restore --staged ."
+abbr --add gamend "git commit --amend"
 
 # OTHER ABBREVIATIONS
 abbr --add vim nvim
@@ -34,6 +34,7 @@ abbr --add v nvim
 abbr --add ts tmux-sessionizer
 abbr --add l eza
 abbr --add ll eza -lha
+abbr --add lt eza --tree --level 3
 abbr --add matrix cmatrix -s -r
 abbr --add stow dots stow
 
