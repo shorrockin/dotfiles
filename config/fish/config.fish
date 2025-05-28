bba80bfe90e74242751957b7eca506355f2612a0
Chris Shorrock <cs@stripe.com>
2025-05-27 21:08:34 -0700

    work tweaks for fish as default shell

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,14 +2,15 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+
 # ran at fish start, disables the default welcome message
 function fish_greeting
 end
 
-# allows us to source a custom work script, checked in seperately
+if command -v starship > /dev/null
+    starship init fish | source
+end
+
 if test -e ~/.work.fish
     source ~/.work.fish
 end
-
-# https://starship.rs/
-starship init fish | source
