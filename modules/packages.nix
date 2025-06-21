c6861425098477191ba20b0719a0b4c3a813346b
Chris Shorrock <chris.shorrock@gmail.com>
2025-06-21 05:48:45 -0700

    removed bambu, need newer version

--- modules/packages.nix
+++ modules/packages.nix
@@ -71,15 +71,15 @@
     mangohud # game overlap for fps
     google-chrome
 
-    (bambu-studio.overrideAttrs (previousAttrs: {
-      version = "01.00.01.50";
-      src = fetchFromGitHub {
-        owner = "bambulab";
-        repo = "BambuStudio";
-        rev = "v01.00.01.50";
-        hash = "sha256-7mkrPl2CQSfc1lRjl1ilwxdYcK5iRU//QGKmdCicK30=";
-      };
-    }))
+    # (bambu-studio.overrideAttrs (previousAttrs: {
+    #   version = "01.00.01.50";
+    #   src = fetchFromGitHub {
+    #     owner = "bambulab";
+    #     repo = "BambuStudio";
+    #     rev = "v01.00.01.50";
+    #     hash = "sha256-7mkrPl2CQSfc1lRjl1ilwxdYcK5iRU//QGKmdCicK30=";
+    #   };
+    # }))
 
     # protonup # https://www.youtube.com/watch?v=qlfm3MEbqYA&t=392s
   ];
