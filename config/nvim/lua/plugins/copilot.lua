c217f79ece3cb5c0a82f99448564fe376919704b
Chris Shorrock <chris.shorrock@gmail.com>
2025-08-05 10:53:54 -0700

    fixed copilot config

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -5,13 +5,12 @@ return {
 	-- "zbirenbaum/copilot.lua",
 	event = "VeryLazy",
 	enabled = true,
-	setup = function()
-		-- require("copilot").setup()
-		vim.keymap.set("i", "<C-i>", 'copilot#Accept("\\<CR>")', {
+	config = function()
+		vim.g.copilot_no_tab_map = true
+		vim.keymap.set("i", "<S-Tab>", 'copilot#Accept("\\<CR>")', {
 			expr = true,
 			replace_keycodes = false,
 			desc = "Copilot: Accept suggestion",
 		})
-		vim.g.copilot_no_tab_map = true
 	end,
 }
