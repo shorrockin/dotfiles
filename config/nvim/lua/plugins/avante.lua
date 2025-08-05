b782f7e54ff218b250c9824425624118c98a0649
Chris Shorrock <chris.shorrock@gmail.com>
2025-08-05 08:19:13 -0700

    avante tweak

--- config/nvim/lua/plugins/avante.lua
+++ config/nvim/lua/plugins/avante.lua
@@ -4,12 +4,24 @@ return {
 	build = "make",
 	enabled = false,
 	opts = {
-		provider = "ollama",
+		provider = "claude",
 		providers = {
-			ollama = {
-				endpoint = "http://127.0.0.1:11434", 
-				model = "gemma3",
+			claude = {
+				endpoint = "https://api.anthropic.com",
+				model = "claude-sonnet-4-20250514",
+				timeout = 30000, -- Timeout in milliseconds
+				extra_request_body = {
+					temperature = 0.75,
+					max_tokens = 20480,
+				},
 			},
-		},
-	},
-}
+			-- provider = "ollama",
+			-- providers = {
+				-- 	ollama = {
+					-- 		endpoint = "http://127.0.0.1:11434", 
+					-- 		model = "gemma3",
+					-- 	},
+					-- },
+				},
+			}
+		}
