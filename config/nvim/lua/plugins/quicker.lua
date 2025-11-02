4a398bc3c5e75632e76531c95751e97e1e0ce4b8
Chris Shorrock <cs@stripe.com>
2025-11-02 14:36:34 -0800

    added quicker for better quicklist stuff

---
+++ config/nvim/lua/plugins/quicker.lua
@@ -0,0 +1,69 @@
+return {
+	-- Quicker: Enhanced quickfix window: https://github.com/stevearc/quicker.nvim
+	"stevearc/quicker.nvim",
+	event = "FileType qf",
+	opts = {
+		-- Enable syntax highlighting for grep results
+		highlight = {
+			treesitter = true,
+			lsp = true,
+		},
+		-- Show context lines above and below quickfix items
+		-- Set to 0 to disable
+		-- context = 3,
+
+		-- Local buffer options for quickfix window
+		opts = {
+			number = true,
+			relativenumber = false,
+			wrap = false,
+		},
+
+		-- Enable editing functionality
+		edit = {
+			enabled = true,
+			autosave = "always", -- Auto-save changes to files
+		},
+
+		-- Trim leading whitespace from results
+		trim_leading_whitespace = true,
+
+		-- Borders for visual separation
+		borders = {
+			vert = "┃",
+			-- Uncomment to customize other borders
+			-- strong_header = "━",
+			-- soft_header = "╍",
+		},
+
+		-- Custom icons for error types
+		-- type_icons = {
+		-- 	E = "",
+		-- 	W = "",
+		-- 	I = "",
+		-- 	N = "",
+		-- 	H = "",
+		-- },
+	},
+	config = function(_, opts)
+		require("quicker").setup(opts)
+
+		-- Keybindings
+		vim.keymap.set("n", "<leader>qq", function()
+			require("quicker").toggle()
+		end, { desc = "Quicker: Toggle [Q]uickfix" })
+
+		vim.keymap.set("n", "<leader>ql", function()
+			require("quicker").toggle({ loclist = true })
+		end, { desc = "Quicker: Toggle [L]oclist" })
+
+		-- Optional: Expand/collapse context in quickfix window
+		vim.keymap.set("n", "<leader>qe", function()
+			require("quicker").expand()
+		end, { desc = "Quicker: [E]xpand context" })
+
+		vim.keymap.set("n", "<leader>qc", function()
+			require("quicker").collapse()
+		end, { desc = "Quicker: [C]ollapse context" })
+	end,
+}
