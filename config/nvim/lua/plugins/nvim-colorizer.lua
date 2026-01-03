3ca04d01112695748d1e6b0aeb716c0a72a7d0c1
Chris Shorrock <chris.shorrock@gmail.com>
2026-01-02 21:45:50 -0800

    added nvim-colorizer

---
+++ config/nvim/lua/plugins/nvim-colorizer.lua
@@ -0,0 +1,8 @@
+return {
+	-- colorizes hex codes in buffers
+	-- https://github.com/catgoose/nvim-colorizer.lua
+	"catgoose/nvim-colorizer.lua",
+	event = "BufReadPre",
+	opts = { -- set to setup table
+	},
+}
