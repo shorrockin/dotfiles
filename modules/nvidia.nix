0f7c5e2a4c46eee8d55e65faa14b41df3345a200
Chris Shorrock <chris.shorrock@gmail.com>
2026-01-04 14:42:40 -0800

    nvidia tweaks to help resume

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -25,7 +25,10 @@
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    # powerManagement.finegrained = false;
+    # NOTE: This requires hardware.nvidia.prime.offload.enable = true; 
+    # which is usually for laptops. Enabling it on desktops can cause 
+    # unresponsiveness or rebuild errors.
+    powerManagement.finegrained = false;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
@@ -33,6 +36,8 @@
     # supported GPUs is at: 
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
+    # Recommended for RTX 20+ series, especially on Wayland, as it moves firmware 
+    # to the GSP for better power management and stability.
     open = true;
 
     # Enable the Nvidia settings menu,
