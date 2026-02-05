b47c9189932ebc3cc282cbf9e3ae8ee49af6b7a2
Chris Shorrock <cs@stripe.com>
2026-02-05 08:43:00 -0800

    some defaults for markdown to wrap

--- config/nvim/lua/cs/autocmd.lua
+++ config/nvim/lua/cs/autocmd.lua
@@ -13,3 +13,13 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.api.nvim_call_function("system", { "pbcopy", vim.api.nvim_call_function("getreg", { ev.regname }) })
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	desc = "Enable line wrapping for Markdown files",
+	group = vim.api.nvim_create_augroup("markdown-wrap", { clear = true }),
+	pattern = "markdown",
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.linebreak = true -- break lines at word boundaries
+	end,
+})
