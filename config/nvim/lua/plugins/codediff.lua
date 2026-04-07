fde7acbddd02bf6a4538f8011e55800d1f238491
Chris Shorrock <cs@stripe.com>
2026-04-07 08:59:28 -0700

    added more usable next/previous hunk hotkeys

--- config/nvim/lua/plugins/codediff.lua
+++ config/nvim/lua/plugins/codediff.lua
@@ -5,6 +5,8 @@ return {
 	cmd = "CodeDiff",
 	keys = {
 		{ "<leader>cd", "<cmd>CodeDiff<CR>", desc = "CodeDiff: [C]ode [D]iff" },
+		{ "]e", function() require("codediff").next_hunk() end, desc = "CodeDiff: Next hunk" },
+		{ "[e", function() require("codediff").prev_hunk() end, desc = "CodeDiff: Previous hunk" },
 	},
 	opts = {},
 }
